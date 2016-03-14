5f17a256d559ba8cd6efc56f8a52054c523772c6
cpagnot3 <mastere1it@FRO-206-20.fro-206.froment.in.iesanetwork.com>
2016-03-14 18:17:42 +0100

    init project

---
+++ global/Object/Concert.php
@@ -0,0 +1,105 @@
+<?php 
+
+
+class Concert {
+
+	//Integer ref="Concert.id_concert"
+	private $id;
+
+	//Datetime ref="Concert.date_concert"
+	private $dateConcert;
+
+	//String ref="Concert.place"
+	private $place;
+
+	//String ref="Concert.city"
+	private $city;
+
+	//String ref="Concert.country"
+	private $country;
+
+	//String ref="Concert.billeterie"
+	private $billeterie;
+
+
+	//Getter/Setter
+
+	//getId
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	//getDateConcert
+	public function getDateConcert()
+	{
+		return $this->dateConcert;
+	}
+
+	//setDateConcert
+	public function setDateConcert($dateConcert)
+	{
+		$this->dateConcert = $dateConcert;
+		return $this;
+	}
+
+	//getPlace
+	public function getPlace()
+	{
+		return $this->place;
+	}
+
+	//setPlace
+	public function setPlace($place)
+	{
+		$this->place = $place;
+		return $this;
+	}
+
+	//getCity()
+	public function getCity()
+	{
+		return $this->city;
+	}
+
+	//setCity()
+	public function setCity($city)
+	{
+		$this->city = $city;
+		return $this;
+	}
+
+	//getCountry()
+	public function getCountry()
+	{
+		return $this->country;
+	}
+
+	//setCountry()
+	public function setCountry($country)
+	{
+		$this->country = $country;
+		return $this;
+	}
+
+	//getBilleterie
+	public function getBilleterie()
+	{
+		return $this->billeterie;
+	}
+
+	//setBilleterie
+	public function setBilleterie($billeterie)
+	{
+		$this->billeterie = $billeterie;
+		return $this;
+	}
+
+	//default construct
+	public function __construct(){
+		
+	}
+
+
+}
+?>
---
+++ global/Repository/ConcertRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+require(dirname(__FILE__).'/../Object/Concert.php');
+require(dirname(__FILE__).'/../Global/Database.php');
+
+class ConcertRepository
+{
+	public function __construct()
+	{
+		# code...
+	}
+	
+}
+
+
+?>
