096cffda0751bf2f454a25d98a76ea17bceee2e1
Mastère 1 IT <Mastre 1 IT>
2016-03-21 16:33:58 +0100

    list news and news by id

--- global/Repository/NewsRepository.php
+++ global/Repository/NewsRepository.php
@@ -22,6 +22,80 @@ class NewsRepository
 		$data = file_put_contents(dirname(__FILE__).'/../JsonData/site.json', $newJsonString);			
 		return $data;
 	}
+
+	public function addNews($news){
+		//get last id 
+		$data = $this->getJson();
+		$newsList = $data->news;
+		foreach ($newsList as $key => $value) {
+			$lastNewsId = $key;
+		}
+		$newsId = $lastNewsId + 1;
+
+		$news = array(
+			'title' => $news->getTitle(),
+			'date'	=> $news->getDate(),
+			'chapo' => $news->getChapo(),
+			'image' => $news->getImage(),
+			'texte'	=> $news->getTexte()
+			);
+
+		$data->news->$newsId = $news;			
+		$newJson = $this->setJson($data);
+		return $newJson;
+	}
+
+	public function getNewsById($id)
+	{
+		$data = $this->getJson();
+		$newsData = $data->news->$id;
+		$news = new News();	
+		$news->setId($id);
+		$news->setTitle($newsData->title);
+		$news->setDate($newsData->date);
+		$news->setChapo($newsData->chapo);
+		$news->setImage($newsData->image);
+		$news->setTexte($newsData->texte);
+
+		return $news;
+
+	}
+
+	public function getListNews($limit)
+	{
+		$data = $this->getJson();
+		$listNews = array();
+		foreach ($data->news as $key => $value) {
+			$lastNewsId = $key;
+		}
+
+		if($limit>$lastNewsId)
+		{
+			$limit = $lastNewsId;
+		}
+
+		$data = $this->getJson();
+		$listNews = array();
+		for($i=($lastNewsId-$limit)+1;$i<=$lastNewsId;$i++){
+			$news = new News();	
+			$news->setId($i);
+			$news->setTitle($data->news->$i->title);
+			$news->setDate($data->news->$i->date);
+			$news->setChapo($data->news->$i->chapo);
+			$news->setImage($data->news->$i->image);
+			$news->setTexte($data->news->$i->texte);
+			$listNews[] = $news;
+		}			
+		return $listNews;
+	}
+
+	public function deleteNews($id)
+	{
+		$data = $this->getJson();
+		unset($data->news->$id);
+		$newJson = $this->setJson($data);
+		return $newJson;		
+	}
 }
 
 ?>
---
+++ site/php/news.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
+	<?php
+		require(dirname(__FILE__).'/../../global/Repository/NewsRepository.php');
+	?>
+	<h1>Liste des news</h1>	
+	<?php
+	$newsRepository = new NewsRepository();
+	if(!isset($_GET['id'])){		
+		foreach ($newsRepository->getListNews(10) as $news) {
+			echo '  <article>
+						<h2>'.$news->getTitle().'</h2>
+					  	<p>'.$news->getDate().'</p>
+					  	<img src="'.$news->getImage().'">
+					  	<p>'.$news->getChapo().'<br/><a href="news.php?id='.$news->getId().'">Lire la news</a></p>
+					</article>	  
+				  ';
+				}
+		}else{
+			$news = $newsRepository->getNewsById($_GET['id']);
+			echo '  <article>
+						<h2>'.$news->getTitle().'</h2>
+					  	<p>'.$news->getDate().'</p>					  	
+					  	<p>'.$news->getChapo().'</p>
+					  	<img src="'.$news->getImage().'">
+					  	'.$news->getTexte().'
+
+					</article>	  
+			';
+		}
+	
+	?>	
+</body>
+
+</html>
