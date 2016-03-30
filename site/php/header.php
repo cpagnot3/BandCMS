bee9ece16ccd061a4d61958970ecc2614573f78d
dimitripana <panayiotou.dimitri@gmail.com>
2016-03-30 16:33:03 +0200

    modifheader

--- site/php/header.php
+++ site/php/header.php
@@ -1,12 +1,12 @@
 <header id="main-header" class="row">
 	<nav class="col-lg-12">
 		<ul>
-			<li><a href="./">accueil</a></li>
-			<li><a href="#accueil">Band</a></li>
-			<li><a href="#apropos">News</a></li>
-			<li><a href="#accueil">Concerts</a></li>
+			<li><a href="../index.php">accueil</a></li>
+			<li><a href="#site/php/Band">Band</a></li>
+			<li><a href="../php/news.php">News</a></li>
+			<li><a href="../php/show.php">Concerts</a></li>
 			<li><a href="#galerie">Galerie</a></li>
-			<li><a href="#contact">Contact</a></li>
+			<li><a href="../php/contact.php">Contact</a></li>
 		</ul>
 	</nav>
 
